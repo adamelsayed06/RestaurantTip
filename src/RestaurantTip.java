0f22628d85e8c2cf6594505ffbbc3a7e0e952711
adamelsayed06 <adame68@nycstudents.net>
2022-09-22 19:05:27 -0400

    cleaned up decimal points and added comments for ambigious pieces of code.

--- src/RestaurantTip.java
+++ src/RestaurantTip.java
@@ -11,11 +11,16 @@ public class RestaurantTip {
         int guestNumber = scan.nextInt();
 
         System.out.println();
+
         System.out.print("How much do you want to tip, as an integer (omit the percent symbol)? ");
         int tipPercent = scan.nextInt();
 
         double currentInput = 0;
         double total = 0;
+
+        /* while the output doesn't equal -1, it repeats and adds every input into a variable named "total" which we
+        need for displaying the ouput, not every individual input is needed.
+        */
 
         while (currentInput != -1){
                 System.out.print("Please enter a cost in dollars and cents (-1 to end) ");
@@ -28,25 +33,42 @@ public class RestaurantTip {
 
         System.out.println("------------------------------------");
 
-        System.out.println("Total Bill Before Tip: " + total);
+        /* whenever you see clean/dirty here, dirty refers to the variable before it is cut down to 2 decimal points,
+        clean refers to after it has been cut down
+        */
+        String totalClean = formatter.format(total);
+
+        System.out.println("Total Bill Before Tip: " + totalClean);
 
         System.out.println("Tip Percentage: " + tipPercent);
 
         double tipDirty = ((total * 1.00)) * (tipPercent * 1.00) / 100;
         String tipClean = formatter.format(tipDirty);
+
         System.out.println("Total Tip: " + (tipClean));
 
+        //this translates the string into a double so we can use it in operations
         double tipClean2 = Double.parseDouble(tipClean);
 
-        System.out.println("Total Bill With Tip: " + ((total) + tipClean2));
+        double totalWithTipDirty = ((total) + tipClean2);
+        String totalWithTipClean = formatter.format(totalWithTipDirty);
 
-        System.out.println("Per Person Cost Before Tip: " + (total/guestNumber));
+        System.out.println("Total Bill With Tip: " + (totalWithTipClean));
 
-        // Format tip per person using the formatter.format so that it is in dollars and cents
+        double perPersonCostBeforeTipDirty = total/guestNumber;
+        String perPersonCostBeforeTipClean = formatter.format(perPersonCostBeforeTipDirty);
 
-        System.out.println("Tip Per Person: " + ((tipClean2) / (guestNumber)));
+        System.out.println("Per Person Cost Before Tip: " + perPersonCostBeforeTipClean);
 
-        System.out.println("Total Cost Per Person: " + (tipClean2 + total) / guestNumber);
+        double tipPersonDirty = (tipClean2) / (guestNumber);
+        String tipPersonClean = formatter.format(tipPersonDirty);
+
+        System.out.println("Tip Per Person: " + (tipPersonClean));
+
+        double totalCostPerPersonDirty = (tipClean2 + total) / guestNumber;
+        String totalCostPerPersonClean = formatter.format(totalCostPerPersonDirty);
+
+        System.out.println("Total Cost Per Person: " + totalCostPerPersonClean);
 
     }
 }
